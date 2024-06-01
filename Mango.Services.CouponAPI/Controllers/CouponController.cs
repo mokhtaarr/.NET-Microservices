878dcd8a7e41ef448d2b519dbad03160c577b6ba
mokhtaarr <ehabmokhtar32@gmail.com>
2024-06-01 19:58:02 +0300

    add frontEnd project

---
+++ Mango.Services.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,150 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDto _response;
+        private IMapper _mapper;
+        public CouponController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _response = new ResponseDto();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> coupons = _db.Coupons;
+                _response.Result = _mapper.Map<IEnumerable<CouponDto>>(coupons);
+            }
+            catch(Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.First(u=>u.CouponId == id);
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+                 
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public ResponseDto GetByCode(string code)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.FirstOrDefault(u => u.CouponCode.ToLower() == code.ToLower());
+                if(coupon == null)
+                {
+                    _response.IsSuccess = false;
+                } 
+
+
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPost]
+        public ResponseDto Post(CouponDto CouponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(CouponDto);
+                _db.Coupons.Add(obj);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(obj);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPut]
+        public ResponseDto put(CouponDto CouponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(CouponDto);
+                _db.Coupons.Update(obj);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(obj);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpDelete]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.Find(id);
+                _db.Coupons.Remove(obj);
+                _db.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+
+    }
+}
